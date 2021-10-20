fb8798bb27a97d2ace2c3be399afd0535769a033
PepeLeChien <fabrizio0943@gmail.com>
2021-10-19 19:06:49 -0500

    calculo de area y perimetro de cuadrados con inputs

---
+++ mates.js
@@ -0,0 +1,68 @@
+// codigo del cuadrado
+
+console.group("Cuadrados");
+
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+
+function areaCuadrado (lado){
+    return lado * lado;
+}
+
+console.groupEnd();
+
+
+
+
+//triangulo 
+
+console.group("Triangulo");
+
+function perimetroTriangulo (lado1,lado2,base) {
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo (base,altura) {
+    return (base * altura) / 2;
+}
+
+console.groupEnd();
+
+
+
+
+//circulo
+
+console.group("Circulo");
+
+function diametroCirculo (radio) {
+    return radio * 2;
+}
+
+const PI = Math.PI;
+
+function perimetroCirculo (radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+function areaCirculo (radio) {
+    return (radio * radio) * PI;
+}
+
+console.groupEnd();
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("input-cuadrado");
+    const lado = input.value;
+    const perimetro = lado * 4;
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("input-cuadrado");
+    const lado = input.value;
+    const area = lado * lado;
+    alert(area);
+}
