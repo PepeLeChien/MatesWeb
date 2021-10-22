f81e82617892843bfd7f5f0971599c90342c9764
PepeLeChien <fabrizio0943@gmail.com>
2021-10-22 00:12:50 -0500

    lista la segunda pagina: porcentajes y descuentos

--- porcentajes/descuentos.js
+++ porcentajes/descuentos.js
@@ -19,3 +19,39 @@ function calcularPrecio () {
     const resultado = document.getElementById("price--result");
     resultado.innerHTML = `El precio del producto con ${porcentajeDescuento}% de descuento es $${precioDescuento}`; 
 }
+
+function calcularCupon() {
+    const precioOriginal = document.getElementById("coup-price--input").value;
+    const cupon = document.getElementById("coup-discount--input").value;
+    
+    const cuponValido = function (coupons) {
+        return coupons.name === cupon;
+    }
+    const cuponUsuario = coupons.find(cuponValido);
+    const resultado = document.getElementById("coup-price--result");
+
+
+    if (!cuponUsuario) {
+        resultado.innerHTML = `No existe el cupón: "${cupon}"`;
+    } else {
+        const porcentajeDescuento = cuponUsuario.discount;
+        const precioDescuento = ((100 - porcentajeDescuento) * precioOriginal) / 100;
+        resultado.innerHTML = `El precio del producto con el cupón "${cupon}" es $${precioDescuento}`;
+    }
+}
+
+const coupons = [
+    {
+        name: "hostinger",
+        discount: 15,
+    },
+    {
+        name: "nemensio",
+        discount: 20,
+    },
+    {
+        name: "zapacosta",
+        discount: 30,
+    }
+]
+
