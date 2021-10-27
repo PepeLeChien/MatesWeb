7c14cd0feeb064147391d8fdd123dc43ab1fb692
PepeLeChien <fabrizio0943@gmail.com>
2021-10-26 19:36:12 -0500

    moda y mediana js terminadas

--- promedios/promedios.js
+++ promedios/promedios.js
@@ -17,4 +17,39 @@ function calcularPromedio (lista) {
 // return = 20;
 
 
+function calcularMediana (arr) {
+    let array = arr.sort((a,b) => a - b);
+    let mediana;
+    let mitadLista = parseInt(array.length / 2); 
+    if (array.length % 2 === 0){
+        elemento1 = array[mitadLista - 1];
+        elemento2 = array[mitadLista];
+        mediana = calcularPromedio([elemento1,elemento2]); 
+    } else {
+        mediana = array[mitadLista];
+    }
+    return mediana;
+}
 
+
+function calcularModa (arr) {
+    let objetcsArr = {};
+    arr.map(function(element){
+        if (objetcsArr[element]) {
+            objetcsArr[element] += 1;
+        } else {
+            objetcsArr[element] = 1;
+        }
+    })
+    
+    let countedNumbers = Object.entries(objetcsArr).sort((a,b) => a[1]-b[1]);
+    let mCN = []; //mCN = mostCountedNumbers
+    
+    countedNumbers.map(function (elemento) {
+        if (elemento[1] === countedNumbers[countedNumbers.length - 1][1]) {
+            mCN.push(elemento[0])        
+        }
+    })
+    
+    return mCN;
+}
