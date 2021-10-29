178560971b1ea70b4d79bd774b0ba72df038a63d
PepeLeChien <fabrizio0943@gmail.com>
2021-10-28 23:31:44 -0500

    diferencia porcentual entre pobre y no pobres promedio

--- salarios/analisis.js
+++ salarios/analisis.js
@@ -1,5 +1,5 @@
 function calcularPromedio(arr){
-    const sumSalaries = arr.reduce( (a,b) => a + b );
+    const sumSalaries = arr.reduce( (a = 0 ,b) => a + b );
     const average = sumSalaries / arr.length;
     return average
 }
@@ -12,7 +12,7 @@ function calcularMedianaListas(arr) {
     
     const middlePosition = parseInt(ascendSalaries.length / 2);
     let median;
-    if (ascendSalaries.lenght % 2 === 0) {
+    if (ascendSalaries.length % 2 === 0) {
         median = ascendSalaries[middlePosition]
     } else {
         let firstMedian = ascendSalaries[middlePosition - 1];
@@ -28,3 +28,38 @@ function calcularPromedioListas(arr) {
     return average;
 }
 
+function calcularPromedioTop10(arr) {
+    const arrSalaries = arr.map( (object) => object.sueldo);
+    const ascendSalaries = arrSalaries.sort( (a,b) => a - b );
+    
+    const spliceStart = Math.round( ( ascendSalaries.length * 90 ) / 100 );
+    const spliceCuts = ascendSalaries.length - spliceStart;
+    const arrTop10 = ascendSalaries.splice(spliceStart, spliceCuts);
+
+    const top10Average = calcularPromedio(arrTop10);
+    return top10Average;
+}
+
+function isPoor(salary) {
+    return salary <= 1000;
+}
+
+function diferenciaPorcentualPobres(arr) {
+    const arrSalaries = arr.map( (object) => object.sueldo);
+
+    const arrPoors = arrSalaries.filter(function(elemento){
+        return isPoor(elemento);
+    });
+    const arrNoPoors = arrSalaries.filter(function(elemento){
+        return !isPoor(elemento);
+    });
+
+    const averagePoors = Math.round(calcularPromedio(arrPoors));
+    const averageNoPoors = Math.round(calcularPromedio(arrNoPoors));
+
+    const noPoorPercentage = ( (averageNoPoors * 100) / averagePoors ).toFixed(2);
+    const percentualDifference = Math.round(noPoorPercentage - 100);
+
+    console.log(`En promedio una persona no pobre gana ${percentualDifference}% más de lo que gana un pobre`);
+    return percentualDifference;
+}
