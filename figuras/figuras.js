f1d6df59984b697ec113521d4d7630d4f5c6c4ad
PepeLeChien <fabrizio0943@gmail.com>
2021-10-31 14:41:06 -0500

    listo html, css y js de la pagina de figuras geometricas en mobile (falta tablet y desktop)

--- figuras/figuras.js
+++ figuras/figuras.js
@@ -1,47 +1,47 @@
 function calcularPerimetroCuadrado() {
-    const lado = document.getElementById("input-cuadrado").value;
+    const lado = document.getElementById("input-square").value;
     const perimetro = lado * 4;
-    const resultado = document.getElementById("figures__square--result");
-    resultado.innerHTML = `El perímetro del cuadrado es ${perimetro}`;
+    const resultado = document.getElementById("result-square");
+    resultado.innerHTML = `El perímetro es ${perimetro}u`;
 }
 
 function calcularAreaCuadrado() {
-    const lado = document.getElementById("input-cuadrado").value;
+    const lado = document.getElementById("input-square").value;
     const area = lado * lado;
-    const resultado = document.getElementById("figures__square--result");
-    resultado.innerHTML = `El área del cuadrado es ${area} u^2`;
+    const resultado = document.getElementById("result-square");
+    resultado.innerHTML = `El área es ${area} u^2`;
 }
 
 function calcularPerimetroTriangulo() {
-    const lado1 = Number(document.getElementById("input-triangulo-lado1").value);
-    const lado2 = Number(document.getElementById("input-triangulo-lado2").value);
-    const base = Number(document.getElementById("input-triangulo-base").value);
+    const lado1 = Number(document.getElementById("input-triangle-1").value);
+    const lado2 = Number(document.getElementById("input-triangle-2").value);
+    const base = Number(document.getElementById("input-triangle-base").value);
     const perimetro = lado1 + lado2 + base;
-    const resultado = document.getElementById("figures__triangle--result");
-    resultado.innerHTML = `El perímetro del triángulo es ${perimetro}`;
+    const resultado = document.getElementById("result-triangle");
+    resultado.innerHTML = `El perímetro es ${perimetro}`;
 }
 
 function calcularAreaTriangulo() {
-    const base = document.getElementById("input-triangulo-base").value;
-    const altura = document.getElementById("input-triangulo-altura").value;
+    const base = document.getElementById("input-triangle-base").value;
+    const altura = document.getElementById("input-triangle-height").value;
     const area = (base * altura) / 2;
-    const resultado = document.getElementById("figures__triangle--result");
-    resultado.innerHTML = `El área del triángulo es ${area} u^2`;
+    const resultado = document.getElementById("result-triangle");
+    resultado.innerHTML = `El área es ${area} u^2`;
 }
 
 function calcularPerimetroCirculo() {
-    const radio = document.getElementById("input-circulo").value;
+    const radio = document.getElementById("input-circle").value;
     const diametro = radio * 2;
     const perimetro = (diametro * Math.PI).toFixed(2);
-    const resultado = document.getElementById("figures__circle--result");
-    resultado.innerHTML = `El perímetro del circulo es ${perimetro}`;
+    const resultado = document.getElementById("result-circle");
+    resultado.innerHTML = `El perímetro es ${perimetro} u`;
 }
 
 function calcularAreaCirculo() {
-    const radio = document.getElementById("input-circulo").value;
+    const radio = document.getElementById("input-circle").value;
     const area = ((radio * radio) * Math.PI).toFixed(2);
-    const resultado = document.getElementById("figures__circle--result");
-    resultado.innerHTML = `El área del circulo es ${area} u^2`;
+    const resultado = document.getElementById("result-circle");
+    resultado.innerHTML = `El área es ${area} u^2`;
 }
 
 function calcularAlturaIsosceles(lado1,lado2,lado3) {
